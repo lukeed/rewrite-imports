6843383a87af4276a2b521e44205ff24402c3f72
Luke Edwards <luke.edwards05@gmail.com>
2019-01-05 17:01:52 -0800

    major: build with destruct'd keys

--- src/index.js
+++ src/index.js
@@ -3,33 +3,32 @@
 const UNNAMED = /import\s*['"]([^'"]+)['"];?/gi;
 const NAMED = /import\s*(\*\s*as)?\s*(\w*?)\s*,?\s*(?:\{([\s\S]*?)\})?\s*from\s*['"]([^'"]+)['"];?/gi;
 
-function alias(key) {
-	key = key.trim();
-	let name = key.split(' as ');
-	(name.length > 1) && (key=name.shift());
-	return { key, name: name[0] };
+function destruct(keys, target) {
+	let tmp, out=[];
+	while (keys.length) {
+		tmp = keys.shift().trim().split(' as ');
+		out.push(tmp.join(':'));
+	}
+	return `const { ${ out.join(', ')} } = ${target};`;
 }
 
 function generate(keys, dep, base, fn) {
-	const tmp = base || (dep.split('/').pop().replace(/\W/g, '_') + '$' + num++); // uniqueness
-	const name = base || alias(tmp).name;
-
 	dep = `${fn}('${dep}')`;
 
-	let obj;
-	let out = `const ${name} = ${dep};`;
+	if (keys.length && !base) {
+		return destruct(keys, dep);
+	}
 
-	keys.forEach(key => {
-		obj = alias(key);
-		out += `\nconst ${obj.name} = ${tmp}.${obj.key};`;
-	});
+	let out = `const ${base} = ${dep};`;
+
+	if (keys.length) {
+		out += '\n' + destruct(keys, base);
+	}
 
 	return out;
 }
 
-let num;
 module.exports = function (str, fn) {
-	num = 0;
 	fn = fn || 'require';
 	return str
 		.replace(NAMED, (_, asterisk, base, req, dep) => generate(req ? req.split(',') : [], dep, base, fn))
