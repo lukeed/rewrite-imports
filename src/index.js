fd5666336889c58467e0e453e7b8cb3557b113d5
Luke Edwards <luke.edwards05@gmail.com>
2019-04-04 00:59:58 -0700

    golf: consolidate & rewrite in ES5

--- src/index.js
+++ src/index.js
@@ -1,36 +1,27 @@
 'use strict';
 
-const UNNAMED = /import\s*['"]([^'"]+)['"];?/gi;
-const NAMED = /import\s*(\*\s*as)?\s*(\w*?)\s*,?\s*(?:\{([\s\S]*?)\})?\s*from\s*['"]([^'"]+)['"];?/gi;
+var UNNAMED = /import\s*['"]([^'"]+)['"];?/gi;
+var NAMED = /import\s*(\*\s*as)?\s*(\w*?)\s*,?\s*(?:\{([\s\S]*?)\})?\s*from\s*['"]([^'"]+)['"];?/gi;
 
 function destruct(keys, target) {
-	let tmp, out=[];
-	while (keys.length) {
-		tmp = keys.shift().trim().split(' as ');
-		out.push(tmp.join(':'));
-	}
-	return `const { ${ out.join(', ')} } = ${target};`;
+	var out=[];
+	while (keys.length) out.push(keys.shift().trim().replace(/ as /g, ':'));
+	return 'const { ' + out.join(', ') + ' } = ' + target + ';';
 }
 
 function generate(keys, dep, base, fn) {
-	dep = `${fn}('${dep}')`;
-
-	if (keys.length && !base) {
-		return destruct(keys, dep);
-	}
-
-	let out = `const ${base} = ${dep};`;
-
-	if (keys.length) {
-		out += '\n' + destruct(keys, base);
-	}
-
-	return out;
+	dep = fn + "('" + dep + "')";
+	if (keys.length && !base) return destruct(keys, dep);
+	return 'const ' + base + ' = ' + dep + ';' + (keys.length ? '\n' + destruct(keys, base) : '');
 }
 
-module.exports = function (str, fn) {
+export default function (str, fn) {
 	fn = fn || 'require';
 	return str
-		.replace(NAMED, (_, asterisk, base, req, dep) => generate(req ? req.split(',') : [], dep, base, fn))
-		.replace(UNNAMED, (_, dep) => `${fn}('${dep}');`);
+		.replace(NAMED, function (x, asterisk, base, req, dep) {
+			return generate(req ? req.split(',') : [], dep, base, fn);
+		})
+		.replace(UNNAMED, function (x, dep) {
+			return (fn + "('" + dep + "');");
+		});
 }
